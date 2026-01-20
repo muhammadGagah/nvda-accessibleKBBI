7d86aca80e9999b07d9bcf3b426cb9ca3c8525e6
Muhammad <muha.aku@gmail.com>
2026-01-20 11:52:36 +0700

    fix some format code

--- addon/installTasks.py
+++ addon/installTasks.py
@@ -1,9 +1,10 @@
 import gui
 import wx
 
+
 def onInstall():
 	gui.messageBox(
 		"Terima kasih telah menginstal Accessible KBBI! Semoga add-on ini membantu Anda belajar dan bekerja lebih produktif. Salam hangat dari Muhammad!",
 		"Accessible KBBI",
-		wx.OK | wx.ICON_INFORMATION
+		wx.OK | wx.ICON_INFORMATION,
 	)
